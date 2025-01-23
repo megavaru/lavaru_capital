0f8f0c6458e0f58ad8df3f7cc87423780c5a61b2
megavaru <195691892+megavaru@users.noreply.github.com>
2025-01-23 22:11:50 +0100

    Update

--- build/lib/lavaru_capital/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.1.0"
-
-from .varu_data import get_data, add_indicators
-
-__all__ = ["get_data", "add_indicators"]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lavaru_capital",
-    version="0.1.0",
+    version="0.1.1",
     author="megavaru",
     author_email="anon@anon.anon",
     description="De la megavaru pentru veri",
